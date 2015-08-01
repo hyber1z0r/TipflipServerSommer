8e839dd86cb1d91ae963b7b1a919cb25d5e11383
hyber1z0r <jakobsky@live.dk>
2015-08-01 17:05:01 +0200

    Made imageValidator a module

---
+++ server/modules/imageValidator.js
@@ -0,0 +1,23 @@
+/**
+ * Created by jakobgaardandersen on 01/08/15.
+ */
+var fs = require('fs');
+
+function isImage(path) {
+    // magicnumbers for jpg, png and bmp
+    // http://www.astro.keele.ac.uk/oldusers/rno/Computing/File_magic.html#Image
+    var magic = ['ffd8ffe0', '89504e47', '424d'];
+    var buffer = fs.readFileSync(path);
+    var magicNumber = buffer.toString('hex', 0, 4);
+    return magic.indexOf(magicNumber) !== -1;
+}
+
+module.exports = function (req, res, next) {
+    if (req.files.image) {
+        if (!isImage(req.files.image.path)) {
+            fs.unlinkSync(req.files.image.path);
+            return res.status(400).json({message: 'File was not an image'});
+        }
+    }
+    next();
+};
--- server/routes/restapi.js
+++ server/routes/restapi.js
@@ -5,25 +5,8 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var datalayer = require('../db/datalayer');
+var imageValidator = require('../modules/imageValidator');
 
-function isImage(path) {
-    // magicnumbers for jpg, png and bmp
-    // http://www.astro.keele.ac.uk/oldusers/rno/Computing/File_magic.html#Image
-    var magic = ['ffd8ffe0', '89504e47', '424d'];
-    var buffer = fs.readFileSync(path);
-    var magicNumber = buffer.toString('hex', 0, 4);
-    return magic.indexOf(magicNumber) !== -1;
-}
-
-var imageValidator = function (req, res, next) {
-    if (req.files.image) {
-        if (!isImage(req.files.image.path)) {
-            fs.unlinkSync(req.files.image.path);
-            return res.status(400).json({message: 'File was not an image'});
-        }
-    }
-    next();
-};
 
 /**
  *  For creating a new category. Requires a name and an image for the category.
